4630bf8e70f3a2919d1b93bb489c9f340dbeacc8
Atharva <atharva.raibagi1@gmail.com>
2023-10-07 20:34:01 +0530

    Fixed Pickle file error

---
+++ app.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import pickle
+
+model = pickle.load(open("C:/Users/Atharva/Desktop/rxib/Diabetes_Prediction_Model_Practice/diabetes_prediction/diabetes_model.sav",'rb'))
+
+print("Loaded")
