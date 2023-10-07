663a4bb82996a0019c47fc32e84f42d6073453be
Atharva <atharva.raibagi1@gmail.com>
2023-10-07 21:30:43 +0530

    Built web app using Streamlit

--- app.py
+++ app.py
@@ -5,4 +5,36 @@ import pickle
 
 model = pickle.load(open("C:/Users/Atharva/Desktop/rxib/Diabetes_Prediction_Model_Practice/diabetes_prediction/diabetes_model.sav",'rb'))
 
-st.title("Diabetes Prediction")
+def diabetes_prediction(input_data):
+    input_data_reshape = np.asarray(input_data).reshape(1,-1)
+    prediction = model.predict(input_data_reshape)
+    if prediction == 1:
+        return "Diabetes Confirmed"
+    else:
+        return "No Diabetes"
+    
+def main():
+    #Give title to page
+    st.title("Diabetes Prediction")
+    
+    #Getting the input values
+    Pregnancies = st.text_input("No. of Pregnancies")
+    Glucose= st.text_input("Glucose Level")
+    BloodPressure = st.text_input("BP Level")
+    SkinThickness= st.text_input("SkinThickness")
+    Insulin= st.text_input("Insulin")
+    BMI= st.text_input("BMI")
+    DiabetesPedigreeFunction =  st.text_input("Diabetes Pedigree Function")
+    Age = st.text_input("Age")
+
+    diagnosis = ''
+
+    if st.button("Test"):
+        diagnosis = diabetes_prediction([Pregnancies,Glucose,BloodPressure,SkinThickness,
+                                         Insulin,BMI,DiabetesPedigreeFunction,Age])
+        
+    st.success(diagnosis)
+
+
+if __name__ == '__main__':
+    main()
